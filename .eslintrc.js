5d8c4325239c7044122ea79388d2b35b451883f7
JamieYang <jm.y.null@gmail.com>
2023-05-02 12:19:15 +0800

    feat: 完善 eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,28 @@ module.exports = {
   env: {
     node: true,
     browser: true,
-    es6: true,
+    es2022: true,
   },
 
-  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2022,
+  },
+
+  extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
 
   globals: {
     GM_addStyle: true,
+    GM_setValue: true,
+    GM_getValue: true,
+    GM_deleteValue: true,
+    GM_openInTab: true,
+    GM_xmlhttpRequest: true,
+  },
+
+  rules: {
+    'no-use-before-define': ['error', { functions: false }],
+    'no-param-reassign': ['error', { props: false }],
+    'no-console': 'off',
   },
 
   overrides: [
